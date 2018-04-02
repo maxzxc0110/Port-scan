5ae26c62dc12d71954f7672974c3df5bc1ce0bfa
Max <273206894@qq.com>
2018-04-02 17:05:56 +0800

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,11 +1,11 @@
 # -*- coding:utf8 -*-
 #!/usr/bin/python
 import socket, time,threading
+import Tkinter as tk
 
-
-#默认端口列表
 portList = ['21','23','80','8080','3128','8081','9080','1080','443','69','22','25','110','7001',
             '9080','9090','3389','8081','1521','1158','2100','1433','1080']
+
 
 #扫描函数
 def scan(ip,port):
@@ -18,28 +18,49 @@ def scan(ip,port):
         s.close()
     except:
         print '端口扫描异常'
-        
-def main(ip):
-    portList = getList()
-    for i in portList:
-        start = i[0]
-        end = i[1]
+
+#主函数
+def main(ip,flag=True):
+    # print flag
+    # exit(0)
+    if flag:  #扫描默认端口列表
         threads = []
-        for x in xrange(int(start),int(end)):  #开启500个线程
+        for x in portList:
             threads.append(threading.Thread(target=scan,args=(ip,int(x),)))
         try:
             for t in threads:
-                t.start()
+                 t.start()
         except:
-            print 'can\'t start new thread'
+              print 'can\'t start new thread'
 
         try:
             for t in threads:
                 t.join()
         except:
-            print 'oops~'
+              print 'oops~'
+    else:  #全端口扫描
+        portList = getList()
+        for i in portList:
+            start = i[0]
+            end = i[1]
+            threads = []
+            for x in xrange(int(start),int(end)):  #开启500个线程
+                threads.append(threading.Thread(target=scan,args=(ip,int(x),)))
+            try:
+                for t in threads:
+                    t.start()
+            except:
+                print 'can\'t start new thread'
 
-#获取端口数组        
+            try:
+                for t in threads:
+                    t.join()
+            except:
+                print 'oops~'
+
+
+
+#获取全端口数组        
 def getList():
     l = []
     s = []
@@ -54,10 +75,15 @@ def getList():
         s = []
         num = num + 500
     return l
+
+
+
     
 if __name__=='__main__':
     ip = raw_input("请输入要扫描的ip:\n")
+
+    #ip = '101.200.139.133' #test data
     print "扫描开始:%s"%ip
     start_time=time.time()
-    main(ip)
+    main(ip,0)
     print '扫描结束，用时：'+str(time.time()-start_time)
